8e0a018bc2cb300f63667781787a0622d55ef736
Jelisa Butler <jelisa.l.butler@gmail.com>
2019-09-18 15:27:19 -0500

    finished start page

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+
+ // Creating variables to hold the number of wins, losses, and ties. They start at 0.
+ var wins = 0;
+ var losses = 0;
+ 
+
+
+var winsText = document.getElementById("wins");
+var lossesText = document.getElementById("loses");
+var guessesLeftText = document.getElementById("guesses-left");
+var guessesSoFarText = document.getElementById("guesses-so-far");
+
+
+
+
+
+// console.log(alpha[1]);
+
+// Setting array for computer to choose
